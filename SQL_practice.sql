1b9865ed3f579e4c365c6a81e8714a45280fbb5b
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-05-12 21:50:04 +0530

    Added query to concatenate first name and last name into a single column to display full names.

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -1,19 +1,23 @@
 # Show first name, last name, and gender of patients whose gender is 'M'
  
-Query: SELECT first_name, last_name, gender  FROM patients WHERE gender ="M"
+    Query: SELECT first_name, last_name, gender  FROM patients WHERE gender ="M"
 
 # Show first name and last name of patients who does not have allergies. (null)
 
-Query: SELECT first_name, last_name FROM patients WHERE allergies is null
+    Query: SELECT first_name, last_name FROM patients WHERE allergies is null
 
 # Show first name of patients that start with the letter 'C'
 
-Query: SELECT first_name FROM patients WHERE first_name like "C%"
+    Query: SELECT first_name FROM patients WHERE first_name like "C%"
 
 #Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
 
-Query: SELECT first_name, last_name FROM patients WHERE weight between 100 and 120;
+    Query: SELECT first_name, last_name FROM patients WHERE weight between 100 and 120;
 
 #Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
 
-Query: update patients SET allergies = "NKA" where allergies is null
+    Query: update patients SET allergies = "NKA" where allergies is null
+
+#Show first name and last name concatinated into one column to show their full name.
+
+   Query: select concat(first_name," ",last_name) AS full_name from patients
