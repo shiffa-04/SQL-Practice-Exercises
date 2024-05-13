b6e4cf4d40ae2a64fbe5db21b5ca29f8cf3ba068
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-05-13 23:17:46 +0530

    Implement SQL query to count patients born in 2010

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -26,3 +26,6 @@
  Example: 'Ontario' instead of 'ON'
 
    Query: select first_name, last_name, province_name from patients join province_names using(province_id);
+
+#Show how many patients have a birth_date with 2010 as the birth year.
+   Query: select count(patient_id) from patients where birth_date like '2010-%%-%%'
