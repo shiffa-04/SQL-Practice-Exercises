5514023fddbe8b739f88783abfa828a40b4d7bd9
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-05-15 22:26:17 +0530

    Implemented query to select all columns for specific patient_ids.

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -32,3 +32,6 @@
 
 #Show the first_name, last_name, and height of the patient with the greatest height.
    Query: select first_name, last_name, height from patients where height = (select max(height) from patients);
+
+#Show all columns for patients who have one of the following patient_ids:1,45,534,879,1000
+   Query: select * from patients where patient_id in (1,45, 534, 879, 1000);
