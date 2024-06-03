54c3bede9267da0ba70a8cb6cf1f787b7ba7c103
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-06-03 22:14:01 +0530

    Retrieve and display patients' first and last names and their allergies for those allergic to Penicillin or Morphine, ordered by allergy type, first name, and last name.

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -63,3 +63,6 @@ Display the two results in the same row.
          sum(CASE WHEN gender = 'F' THEN 1 ELSE 0 END) AS female_count
       FROM 
          patients;
+#Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
+   Query: select first_name, last_name, allergies from patients where allergies = 'Penicillin' or allergies= 'Morphine' 
+order by allergies , first_name, last_name
