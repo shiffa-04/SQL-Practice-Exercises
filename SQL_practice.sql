22d1e3ecb8041bf358862fceb146699e8a37ec9d
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-06-20 22:26:19 +0530

    Add files via upload
    Display patients full names with formatted last and first names, ordered by first name in descending order

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -96,3 +96,6 @@ The roles are either "Patient" or "Doctor"
          FROM patients
          GROUP BY province_id
          HAVING SUM(height) >= 7000;
+
+#We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order.
+   Query: select concat(upper(last_name),"," ,lower(first_name)) As FULL_NAME from patients order by first_name Desc;
