56acc118a814c09153ffaf3a8c6a4e4769cba242
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-07-07 22:41:14 +0530

    Add SQL query to display patient's full name, height in feet (rounded to 1 decimal), weight in pounds (rounded to 0 decimals), birth date, and non-abbreviated gender. Converts height from cm to feet and weight from kg to pounds.

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -139,3 +139,17 @@ Query:select first_name, last_name, count(*) as num_of_duplicates
       from patients
       group by first_name, last_name
       having count(*) > 1
+
+#Display patients full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated.
+Convert CM to feet by dividing by 30.48.Convert KG to pounds by multiplying by 2.205.
+Query:SELECT 
+      concat(first_name, " ", last_name) AS patient_full_name,
+      ROUND(height / 30.48, 1) AS height_feet,
+      ROUND(weight * 2.205, 0) AS weight_pounds,
+      birth_date,
+      CASE 
+         WHEN gender = 'M' THEN 'Male'
+         WHEN gender = 'F' THEN 'Female'
+         ELSE 'Other'
+      END AS gender
+      FROM patients;
