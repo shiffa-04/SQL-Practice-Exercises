1e78d6fe4850dc7ddb59cb42f0f5b645a59f6ad9
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-07-10 23:06:21 +0530

    Add query to group patients by weight ranges and count total patients in each group, ordered by weight group descending

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -158,3 +158,15 @@ Query:SELECT
 Query: select p.patient_id, p.first_name, p.last_name 
        from patients p 
        where p.patient_id not in (SELECT a.patient_id FROM admissions a)
+
+#Show all of the patients grouped into weight groups.Show the total amount of patients in each weight group.Order the list by the weight group decending.
+For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+Query: SELECT 
+         FLOOR(Weight / 10) * 10 AS WeightGroup, 
+         COUNT(*) AS TotalPatients
+      FROM 
+         patients
+      GROUP BY 
+         WeightGroup
+      ORDER BY 
+         WeightGroup DESC;
