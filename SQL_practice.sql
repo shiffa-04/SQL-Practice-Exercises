cd1c3aed8bbb83aa84365863285820f1ec7887c5
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-07-13 22:08:08 +0530

    Add query to display patient_id, weight, height, and isObese status
    - Query includes calculation of BMI using weight in kg and height in cm.
    - Defines obesity based on BMI >= 30.
    - Displays isObese as a boolean (0 or 1).

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -170,3 +170,15 @@ Query: SELECT
          WeightGroup
       ORDER BY 
          WeightGroup DESC;
+
+#Show patient_id, weight, height, isObese from the patients table.Display isObese as a boolean 0 or 1.
+Obese is defined as weight(kg)/(height(m)2) >= 30.weight is in units kg.height is in units cm.
+Query: SELECT 
+         patient_id, 
+         weight, 
+         height, 
+         CASE 
+            WHEN weight / ((height / 100.0) * (height / 100.0)) >= 30 THEN 1 
+            ELSE 0 
+         END AS isObese
+      FROM patients;
