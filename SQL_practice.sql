7edfcab5f7fe015ff8bb254406be5bd3fcbad2a2
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-07-24 22:57:06 +0530

    Add SQL query to generate temporary passwords for patients based on their first admission

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -199,3 +199,17 @@ Query:SELECT
       WHERE 
          a.diagnosis = 'Epilepsy' 
          AND d.first_name = 'Lisa';
+
+#All patients who have gone through admissions, can see their medical documents on our site. Those patients are given a temporary password after their first admission. Show the patient_id and temp_password.
+The password must be the following, in order:
+1. patient_id
+2. the numerical length of patient's last_name
+3. year of patient's birth_date
+
+Query:SELECT DISTINCT 
+         a.patient_id, 
+         CONCAT(a.patient_id, LENGTH(p.last_name), YEAR(p.birth_date)) AS temp_password
+      FROM 
+         admissions a
+      JOIN 
+         patients p ON p.patient_id = a.patient_id;
