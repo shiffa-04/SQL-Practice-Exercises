4958efd2b6f1b635c61ab0baadf103867ea7b57c
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-07-24 22:39:52 +0530

    Add SQL query to show patient_id, first_name, last_name, and attending doctor's specialty for patients diagnosed with 'Epilepsy' and attended by doctor named 'Lisa'

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -182,3 +182,20 @@ Query: SELECT
             ELSE 0 
          END AS isObese
       FROM patients;
+
+#Show patient_id, first_name, last_name, and attending doctor's specialty. Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+Check patients, admissions, and doctors tables for required information. 
+Query:SELECT 
+         p.patient_id, 
+         p.first_name, 
+         p.last_name, 
+         d.specialty
+      FROM 
+         patients p
+      JOIN 
+         admissions a ON a.patient_id = p.patient_id
+      JOIN 
+         doctors d ON a.attending_doctor_id = d.doctor_id
+      WHERE 
+         a.diagnosis = 'Epilepsy' 
+         AND d.first_name = 'Lisa';
