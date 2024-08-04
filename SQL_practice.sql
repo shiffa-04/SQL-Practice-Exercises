522bd27fbc4ddadafc3a59a48733533c4e652cc0
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-08-04 22:50:15 +0530

    Add query to select provinces with more male patients than female patients

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -228,3 +228,12 @@ FROM (
     FROM admissions
 ) AS patient_info
 GROUP BY has_insurance;
+
+#Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name
+
+Query:SELECT province_name
+      FROM province_names
+      JOIN patients 
+      ON province_names.province_id = patients.province_id
+      GROUP BY province_name
+      HAVING SUM(CASE WHEN gender = 'M' THEN 1 ELSE 0 END) > SUM(CASE WHEN gender = 'F' THEN 1 ELSE 0 END);
