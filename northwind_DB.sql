f157b5d22084b413cc1d685cb7b246b8b3facb46
shiffa-04 <152057425+shiffa-04@users.noreply.github.com>
2024-08-23 22:44:33 +0530

    Retrieve and sort category names with descriptions from the categories table in alphabetical order

---
+++ northwind_DB.sql
@@ -0,0 +1,4 @@
+#Show the category_name and description from the categories table sorted by category_name.
+Query:  select category_name, description 
+        from categories 
+        order by category_name;
